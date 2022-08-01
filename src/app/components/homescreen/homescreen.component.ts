c00dbb97da8e22d800e5880ccc5baeecd26f6dfe
Rohan Kadam <rohan.kadam@mindbowser.com>
2022-08-01 14:24:43 +0530

    added min and max to buy now input

--- src/app/components/homescreen/homescreen.component.ts
+++ src/app/components/homescreen/homescreen.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductApiService } from '../../services/product-api.service';
 
-
 @Component({
   selector: 'app-homescreen',
   templateUrl: './homescreen.component.html',
@@ -10,7 +9,13 @@ import { ProductApiService } from '../../services/product-api.service';
 export class HomescreenComponent implements OnInit {
   constructor(private _productApi: ProductApiService) {}
 
-  ngOnInit(): void {}
+  products!: any;
+  ngOnInit(): void {
+    this._productApi.getProduct().subscribe((product) => {
+      this.products = product[0];
+      console.log(this.products);
+    });
+  }
   checkout(quantity: any) {
     this._productApi.checkout(quantity.value).subscribe((url) => {
       window.location.href = url;
